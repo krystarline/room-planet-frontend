e65f7a6b31f2902a6fca5f446a1adfa8fb8b0755
Sujeong Park <krystarline@gmail.com>
2022-07-13 00:42:06 +0900

    feat: implement pageback warning function

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -2,11 +2,14 @@
 import React from "react";
 
 import Container from "@mui/material/Container";
+import { useBeforeunload } from "react-beforeunload";
 
 import AppContents from "../AppContents";
 import AppNavBar from "../AppNavBar";
 
 function Plan() {
+  useBeforeunload((event) => event.preventDefault());
+
   return (
     <Container fixed>
       <AppNavBar />
