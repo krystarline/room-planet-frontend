b79f434fb4172db6a0dd91530aff7135de779809
Sujeong Park <krystarline@gmail.com>
2022-07-19 01:26:49 +0900

    feat: implement app footer functions

--- src/AppFooter.jsx
+++ src/AppFooter.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { Container, Typography, IconButton } from "@mui/material";
+/* eslint-disable no-alert */
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState } from "react";
+
+import {
+  Box,
+  Container,
+  Divider,
+  IconButton,
+  Modal,
+  Typography,
+} from "@mui/material";
+
 import EmailIcon from "@mui/icons-material/Email";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
+const style = {
+  display: "grid",
+  gap: "5%",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function AppFooter() {
-  const handleGithubClick = () => {};
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleCopyClipBoard = async () => {
+    await navigator.clipboard.writeText("krystarline@gmail.com");
+    alert("이메일 복사가 완료되었습니다!");
+  };
+  const handleGithubClick = () => {
+    window.location.href =
+      "https://github.com/krystarline/room-planet-frontend";
+
+    return null;
+  };
 
   return (
-    <Container fixed sx={{ display: "flex", height: "100%" }}>
+    <Container fixed sx={{ display: "flex", height: "100%", padding: "1%" }}>
       <Typography
         variant="body2"
         color="#aba9a6"
@@ -21,15 +60,54 @@ function AppFooter() {
         edge="start"
         color="inherit"
         aria-label="email"
+        title="Email"
         sx={{ mr: 2 }}
+        onClick={handleOpen}
       >
         <EmailIcon />
       </IconButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h5"
+            component="h2"
+            align="center"
+          >
+            📨 krystarline@gmail.com
+            <IconButton
+              size="medium"
+              edge="end"
+              color="inherit"
+              aria-label="text-clipboard"
+              title="Copy"
+              sx={{ mr: 2 }}
+              onClick={handleCopyClipBoard}
+            >
+              <AttachFileIcon />
+            </IconButton>
+          </Typography>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2 }}
+            align="center"
+          >
+            Dev. Sujeong Park
+          </Typography>
+          <Divider />
+        </Box>
+      </Modal>
       <IconButton
         size="large"
         edge="start"
         color="inherit"
         aria-label="github"
+        title="Github"
         sx={{ mr: 2 }}
         onClick={handleGithubClick}
       >
