ec017e9a38aca913997270d8038230e52d6d784b
Sujeong Park <krystarline@gmail.com>
2022-07-22 18:32:26 +0900

    refactor: remove unused component

--- src/components/Keyboard.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/named */
-import { useKeys } from "../hooks/useKeys";
-
-function Keyboard({ handleKeyPress }) {
-  useKeys(handleKeyPress);
-  // return null;
-}
-
-export default Keyboard;
